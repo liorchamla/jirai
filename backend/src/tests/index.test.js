4c3597cc7fbd65a1e70d637da7d9d57670af8c06
Vincent Delaye <delayevincent85@gmail.com>
2025-07-03 10:30:16 +0200

    chore: add unit test (WEB-44)

---
+++ backend/src/tests/index.test.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe("Fonctionnalités du backend", () => {
+  it("devrait retourner vrai pour 1 + 1", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
