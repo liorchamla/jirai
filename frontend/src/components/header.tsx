ec3118d980b0581ab553e385cbe14d1e0a8e2bae
Vincent Delaye <delayevincent85@gmail.com>
2025-07-08 09:31:45 +0200

    chore(frontend): add primereact

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,7 +1,11 @@
+import { Button } from "primereact/button";
+
 function Header() {
   return (
     <header>
       <h1>Salut les devs</h1>
+      <Button label="Click Me" />
+      <i className="pi pi-user"></i>
     </header>
   );
 }
