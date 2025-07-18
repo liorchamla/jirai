d1856c5b82499db022abb0d70385d4941b78d664
Vincent Delaye <delayevincent85@gmail.com>
2025-07-18 11:58:06 +0200

    feat(frontend): add env file and api utils (WEB-64)

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,5 +1,13 @@
 import wretch from "wretch";
 
-export const api = wretch("http://localhost:3000/api/v1", {
+let api = wretch(import.meta.env.VITE_API_URL + "/api/v1", {
   mode: "cors",
 }).errorType("json");
+
+export const getApi = () => {
+  return api;
+};
+
+export const setToken = (token: string) => {
+  api = api.headers({ Authorization: `Bearer ${token}` });
+};
