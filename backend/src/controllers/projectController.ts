bf453c32476b0e5324a14029186d3b5b908666c7
Vincent Delaye <delayevincent85@gmail.com>
2025-07-21 11:45:05 +0200

    feat(backend): add project model and CRUD (WEB-18)

---
+++ backend/src/controllers/projectController.ts
@@ -0,0 +1,175 @@
+import {
+  createProjectSchema,
+  updateProjectSchema,
+} from "../schemas/projectsSchema";
+import prisma from "../utils/prisma";
+import { Request, Response } from "express";
+import slug from "slug";
+
+// ============================================================
+// ====================== GET ALL PROJECTS =====================
+// ============================================================
+
+export async function getAllProjects(req: Request, res: Response) {
+  try {
+    const projects = await prisma.project.findMany({
+      include: { creator: true }, // Include user information if needed
+      omit: { createdBy: true }, // Omit createdBy if not needed in response
+    });
+    res.status(200).json({ projects });
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+// ============================================================
+// ====================== GET PROJECT BY SLUG ==================
+// ============================================================
+
+export async function getProjectBySlug(req: Request, res: Response) {
+  const { slug } = req.params;
+  try {
+    const project = await prisma.project.findUnique({
+      where: { slug },
+      include: { creator: true }, // Include user information if needed
+      omit: { createdBy: true }, // Omit createdBy if not needed in response
+    });
+    if (!project) {
+      res.status(404).json({ error: "Project not found" });
+      return;
+    }
+    res.status(200).json({ project });
+  } catch (error) {
+    console.error("Error fetching project:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+// ============================================================
+// ====================== CREATE PROJECT =======================
+// ============================================================
+
+export async function createProject(req: Request, res: Response) {
+  const result = createProjectSchema.safeParse(req.body);
+  if (!result.success) {
+    res.status(400).json({ error: result.error });
+    return;
+  }
+
+  if (!req.user) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  const { name, description } = result.data;
+  const projectSlug = slug(name);
+
+  try {
+    const newProject = await prisma.project.create({
+      data: {
+        slug: projectSlug,
+        name,
+        description,
+        createdBy: req.user.uuid, // Assuming req.user is set by authentication middleware
+        status: "active", // Default status
+      },
+      include: {
+        creator: true, // Include user information if needed
+      },
+    });
+    res.status(201).json({ project: newProject });
+  } catch (error) {
+    console.error("Error creating project:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+// ============================================================
+// ====================== UPDATE PROJECT =======================
+// ============================================================
+
+export async function updateProject(req: Request, res: Response) {
+  const { slug } = req.params;
+  const result = updateProjectSchema.safeParse(req.body);
+  if (!result.success) {
+    res.status(400).json({ error: result.error });
+    return;
+  }
+
+  if (!req.user) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  if (!slug) {
+    res.status(400).json({ error: "Project slug is required" });
+    return;
+  }
+
+  const existingProject = await prisma.project.findUnique({
+    where: { slug },
+  });
+  if (!existingProject) {
+    res.status(404).json({ error: "Project not found" });
+    return;
+  }
+
+  const { name, description } = result.data;
+
+  try {
+    const updatedProject = await prisma.project.update({
+      where: { slug },
+      data: {
+        name,
+        description,
+      },
+      include: {
+        creator: true,
+      },
+    });
+    res.status(200).json({ project: updatedProject });
+  } catch (error) {
+    console.error("Error updating project:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+// ============================================================
+// ====================== DELETE PROJECT =======================
+// ============================================================
+
+export async function deleteProject(req: Request, res: Response) {
+  const { slug } = req.params;
+
+  if (!req.user) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  if (!slug) {
+    res.status(400).json({ error: "Project slug is required" });
+    return;
+  }
+
+  const existingProject = await prisma.project.findUnique({
+    where: { slug },
+  });
+  if (!existingProject) {
+    res.status(404).json({ error: "Project not found" });
+    return;
+  }
+
+  try {
+    const deletedProject = await prisma.project.delete({
+      where: { slug },
+    });
+    res.status(204).json({
+      message: "Project deleted successfully",
+      project: deletedProject,
+    });
+  } catch (error) {
+    console.error("Error deleting project:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
